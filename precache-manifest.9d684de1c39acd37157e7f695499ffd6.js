518d4824350bfd61dfd7cdc70c01974312619231
Евгений Ращупкин <darrenclyde1@gmail.com>
2019-06-20 15:57:32 +0400

    Updates

---
+++ precache-manifest.9d684de1c39acd37157e7f695499ffd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e63988c651c47ed120b48753e7a531e",
+    "url": "/netology-test/index.html"
+  },
+  {
+    "revision": "eb25cbe96af5ea77d7f8",
+    "url": "/netology-test/static/css/main.0dffb91b.chunk.css"
+  },
+  {
+    "revision": "6fd948e8a2a725953478",
+    "url": "/netology-test/static/js/2.7aadd51a.chunk.js"
+  },
+  {
+    "revision": "eb25cbe96af5ea77d7f8",
+    "url": "/netology-test/static/js/main.1f6668df.chunk.js"
+  },
+  {
+    "revision": "40449b6ba82517b5cffd",
+    "url": "/netology-test/static/js/runtime~main.edcd8d2e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/netology-test/precache-manifest.9d684de1c39acd37157e7f695499ffd6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/netology-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
